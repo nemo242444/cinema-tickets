25457a1ca90f1ef9c469e92cef72d23f7f9bb40a
Nemo <naeem_afzal@icloud.com>
2023-01-30 09:45:38 +0000

    Add in impl of validateTicketRequest
    Validated with any incoming requests having atleast one ADULT &
    TicketTypeRequest object always having 1 or more tickets

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceUtil.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceUtil.java
@@ -1,8 +1,31 @@
 package uk.gov.dwp.uc.pairtest;
 
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+
+import java.util.Arrays;
+
+import static uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type.ADULT;
+
 public class TicketServiceUtil {
 
     public boolean validateId(Long accountId) {
         return accountId.compareTo(1L) >= 0;
+    }
+
+    public boolean validateTicketRequest(TicketTypeRequest... ticketTypeRequests) {
+        return shouldContainAdultInTicketRequests(ticketTypeRequests) &&
+                shouldOnlyContainRequestWith1PlusTickets(ticketTypeRequests);
+    }
+
+    private boolean shouldContainAdultInTicketRequests(TicketTypeRequest... ticketTypeRequests) {
+        return Arrays.stream(ticketTypeRequests).anyMatch(
+                ticketTypeRequest -> ADULT.equals(ticketTypeRequest.getTicketType())
+        );
+    }
+
+    private boolean shouldOnlyContainRequestWith1PlusTickets(TicketTypeRequest... ticketTypeRequests) {
+        return Arrays.stream(ticketTypeRequests).allMatch(
+                ticketTypeRequest -> ticketTypeRequest.getNoOfTickets() > 0
+        );
     }
 }
