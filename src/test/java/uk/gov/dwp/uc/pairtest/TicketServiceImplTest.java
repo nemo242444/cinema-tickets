4567eb1595854c317f40f97b8da5b48e369258c2
Nemo <naeem_afzal@icloud.com>
2023-01-30 09:41:07 +0000

    Add tests to validate TicketTypeRequest fields

--- src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
+++ src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -18,4 +18,52 @@ public class TicketServiceImplTest {
         assertEquals(false, ticketServiceUtil.validateId(0L));
     }
 
+    @Test
+    public void Should_ReturnTrue_When_TicketRequestHasAdult() {
+        TicketTypeRequest ticketRequestAdult = new TicketTypeRequest(ADULT, 1);
+        assertEquals(
+                true,
+                ticketServiceUtil.validateTicketRequest(ticketRequestAdult)
+        );
+    }
+
+    @Test
+    public void Should_ReturnTrue_When_TicketRequestHasAdultWithInfant() {
+        TicketTypeRequest ticketRequestAdult = new TicketTypeRequest(ADULT, 1);
+        TicketTypeRequest ticketRequestInfant = new TicketTypeRequest(INFANT, 1);
+        assertEquals(
+                true,
+                ticketServiceUtil.validateTicketRequest(ticketRequestAdult, ticketRequestInfant)
+        );
+    }
+
+    @Test
+    public void Should_ReturnFalse_When_TicketRequestHasNoTickets() {
+        TicketTypeRequest ticketRequestAdult = new TicketTypeRequest(ADULT, 0);
+        assertEquals(
+                false,
+                ticketServiceUtil.validateTicketRequest(ticketRequestAdult)
+        );
+    }
+
+    @Test
+    public void Should_ReturnFalse_When_TicketRequestHasChildWithoutAdult() {
+        TicketTypeRequest ticketRequestChild = new TicketTypeRequest(CHILD, 1);
+        assertEquals(
+                false,
+                ticketServiceUtil.validateTicketRequest(ticketRequestChild)
+        );
+    }
+
+    @Test
+    public void Should_ReturnFalse_When_TicketRequestHasChildWithInfant() {
+        TicketTypeRequest ticketRequestChild = new TicketTypeRequest(CHILD, 1);
+        TicketTypeRequest ticketRequestInfant = new TicketTypeRequest(INFANT, 1);
+        assertEquals(
+                false,
+                ticketServiceUtil.validateTicketRequest(ticketRequestChild, ticketRequestInfant)
+        );
+    }
+
+
 }
