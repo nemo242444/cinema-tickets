fe9eaff40bc5555d78dba49095302d2c3f960709
Nemo <naeem_afzal@icloud.com>
2023-01-30 10:20:49 +0000

    Update Utils Test class for code cleanup

--- src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceUtilTest.java
+++ src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceUtilTest.java
@@ -3,68 +3,53 @@ package uk.gov.dwp.uc.pairtest;
 import org.junit.Test;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type.*;
 
-public class TicketServiceImplTest {
+public class TicketServiceUtilTest {
 
-    private TicketServiceUtil ticketServiceUtil = new TicketServiceUtil();
+    private final TicketServiceUtil ticketServiceUtil = new TicketServiceUtil();
 
     @Test
     public void Should_ReturnTrue_When_ValidAccountId() {
-        assertEquals(true, ticketServiceUtil.validateId(1L));
+        assertTrue(ticketServiceUtil.validateId(1L));
     }
 
     @Test
     public void Should_ReturnFalse_When_InvalidAccountId() {
-        assertEquals(false, ticketServiceUtil.validateId(0L));
+        assertFalse(ticketServiceUtil.validateId(0L));
     }
 
     @Test
     public void Should_ReturnTrue_When_TicketRequestHasAdult() {
         TicketTypeRequest ticketRequestAdult = new TicketTypeRequest(ADULT, 1);
-        assertEquals(
-                true,
-                ticketServiceUtil.validateTicketRequest(ticketRequestAdult)
-        );
+        assertTrue(ticketServiceUtil.validateTicketRequest(ticketRequestAdult));
     }
 
     @Test
     public void Should_ReturnTrue_When_TicketRequestHasAdultWithInfant() {
         TicketTypeRequest ticketRequestAdult = new TicketTypeRequest(ADULT, 1);
         TicketTypeRequest ticketRequestInfant = new TicketTypeRequest(INFANT, 1);
-        assertEquals(
-                true,
-                ticketServiceUtil.validateTicketRequest(ticketRequestAdult, ticketRequestInfant)
-        );
+        assertTrue(ticketServiceUtil.validateTicketRequest(ticketRequestAdult, ticketRequestInfant));
     }
 
     @Test
     public void Should_ReturnFalse_When_TicketRequestHasNoTickets() {
         TicketTypeRequest ticketRequestAdult = new TicketTypeRequest(ADULT, 0);
-        assertEquals(
-                false,
-                ticketServiceUtil.validateTicketRequest(ticketRequestAdult)
-        );
+        assertFalse(ticketServiceUtil.validateTicketRequest(ticketRequestAdult));
     }
 
     @Test
     public void Should_ReturnFalse_When_TicketRequestHasChildWithoutAdult() {
         TicketTypeRequest ticketRequestChild = new TicketTypeRequest(CHILD, 1);
-        assertEquals(
-                false,
-                ticketServiceUtil.validateTicketRequest(ticketRequestChild)
-        );
+        assertFalse(ticketServiceUtil.validateTicketRequest(ticketRequestChild));
     }
 
     @Test
     public void Should_ReturnFalse_When_TicketRequestHasChildWithInfant() {
         TicketTypeRequest ticketRequestChild = new TicketTypeRequest(CHILD, 1);
         TicketTypeRequest ticketRequestInfant = new TicketTypeRequest(INFANT, 1);
-        assertEquals(
-                false,
-                ticketServiceUtil.validateTicketRequest(ticketRequestChild, ticketRequestInfant)
-        );
+        assertFalse(ticketServiceUtil.validateTicketRequest(ticketRequestChild, ticketRequestInfant));
     }
 
     @Test
